4ab18e5727a669bac18346faa95e8d0be76f110f
fabiofa87 <fabiofadev87@gmail.com>
2023-07-01 15:05:58 -0300

    criado order repository

--- src/domain/entity/customer.ts
+++ src/domain/entity/customer.ts
@@ -1,8 +1,8 @@
-import { Address } from "./address";
+import Address from "./address";
 
-export class Customer {
+export default class Customer {
   private _id: string;
-  private _name = "";
+  private _name: string = "";
   private _address!: Address;
   private _active: boolean = false;
   private _rewardPoints: number = 0;
@@ -13,28 +13,25 @@ export class Customer {
     this.validate();
   }
 
+  get id(): string {
+    return this._id;
+  }
+
   get name(): string {
     return this._name;
   }
 
-  get id(): string {
-    return this._id;
-  }
   get rewardPoints(): number {
     return this._rewardPoints;
   }
 
   validate() {
-    if (this._name.length === 0) {
-      throw new Error("Name is required");
-    }
     if (this._id.length === 0) {
       throw new Error("Id is required");
     }
-  }
-
-  isActive(): boolean {
-    return this._active;
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
   }
 
   changeName(name: string) {
@@ -42,14 +39,27 @@ export class Customer {
     this.validate();
   }
 
-  deactivate() {
-    this._active = false;
+  get Address(): Address {
+    return this._address;
   }
+
+  changeAddress(address: Address) {
+    this._address = address;
+  }
+
+  isActive(): boolean {
+    return this._active;
+  }
+
   activate() {
     if (this._address === undefined) {
-      throw new Error("Address is required");
+      throw new Error("Address is mandatory to activate a customer");
     }
     this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
   }
 
   addRewardPoints(points: number) {
--- src/domain/entity/order_item.ts
+++ src/domain/entity/order_item.ts
@@ -1,14 +1,30 @@
 export default class OrderItem {
   private _id: string;
   private _name: string;
+  private _productId: string;
   private _price: number;
   private _quantity: number;
 
-  constructor(id: string, name: string, price: number, quantity: number) {
+  constructor(
+    id: string,
+    name: string,
+    price: number,
+    productId: string,
+    quantity: number
+  ) {
     this._id = id;
     this._name = name;
     this._price = price;
+    this._productId = productId;
     this._quantity = quantity;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
   }
 
   get price(): number {
@@ -17,6 +33,10 @@ export default class OrderItem {
 
   get quantity(): number {
     return this._quantity;
+  }
+
+  get productId(): string {
+    return this._productId;
   }
 
   orderItemTotal(): number {
